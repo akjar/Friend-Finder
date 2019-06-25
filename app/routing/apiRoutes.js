e5bafbcf321d7a09c74eb056cd59a7cafb7a27b2
Andrew Kjar <andrewkjar34@gmail.com>
2019-06-24 22:16:56 -0700

    updated to vanilla js and fixed some linter issues

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -3,57 +3,54 @@
 // =============================================================================
 
 // require the data from the friends file
-var friends = require("../data/friends.js");
+var friends = require('../data/friends.js')
 
 // =============================================================================
 // 2. make routes to the different html pages
 // =============================================================================
 
 // access app from other file
-module.exports = function(app) {
+module.exports = function (app) {
+  // displays the survey page
+  app.get('/api/friends', function (req, res) {
+    res.json(friends)
+  })
 
-    // displays the survey page
-    app.get("/api/friends", function(req, res) {
-        res.json(friends);
-    });
-    
-    // posting the data
-    app.post("/api/friends", function(req, res) {
-        // create a variable 
-        var bestMatch = {
-            name: "",
-            photo: "",
-            friendDifference: 1000
-        };
+  // posting the data
+  app.post('/api/friends', function (req, res) {
+    // create a variable
+    var bestMatch = {
+      name: '',
+      photo: '',
+      friendDifference: 1000
+    }
 
-        // create variables for results from the survey
-        var userData = req.body;
-        var userScores = userData.scores;
+    // create variables for results from the survey
+    var userData = req.body
+    var userScores = userData.scores
 
-        // create a variable for the score difference
-        var totalDifference = 0;
+    // create a variable for the score difference
+    var totalDifference = 0
 
-        // go through all of the friends
-        for (var i = 0; i < friends.length; i++) {
+    // go through all of the friends
+    for (var i = 0; i < friends.length; i++) {
+      // go through the friends scores
+      for (var j = 0; j < friends[i].scores[j]; j++) {
+        // calculate the difference of scores
+        totalDifference += Math.abs(parseInt(userScores[j]) - parseInt(friends[i].scores[j]))
 
-            // go through the friends scores
-            for (var j = 0; j < friends[i].scores[j]; j++) {
-
-                // calculate the difference of scores
-                totalDifference += Math.abs(parseInt(userScores[j]) - parseInt(friends[i].scores[j]));
-
-                // if the new difference socre is less then the current match then change the match with the new friends
-                if (totalDifference <= bestMatch.friendDifference) {
-                    bestMatch.name = friends[i].name;
-                    bestMatch.photo = friends[i].photo;
-                    bestMatch.friendDifference = totalDifference;
-                }
-            }
+        // if the new difference socre is less then the current match then change the match with the new friends
+        if (totalDifference <= bestMatch.friendDifference) {
+          bestMatch.name = friends[i].name
+          bestMatch.photo = friends[i].photo
+          bestMatch.friendDifference = totalDifference
         }
-        // push the data to the database
-        friends.push(userData);
+      }
+    }
+    // push the data to the database
+    friends.push(userData)
 
-        // turn best match into json format
-        res.json(bestMatch);
-    })
-};
+    // turn best match into json format
+    res.json(bestMatch)
+  })
+}
--- server.js
+++ server.js
@@ -3,37 +3,37 @@
 // =============================================================================
 
 // requiring express
-var express = require("express");
+var express = require('express')
 
 // =============================================================================
 // 2. set up my server properties
 // =============================================================================
 
 // create an express server
-var app = express();
+var app = express()
 
 // set up a port
-var PORT = process.env.PORT || 8080;
+var PORT = process.env.PORT || 8080
 
 // allow app to parse the data
-app.use(express.urlencoded({extended: true}));
-app.use(express.json());
+app.use(express.urlencoded({ extended: true }))
+app.use(express.json())
 
 // =============================================================================
 // 3. require our route files
 // =============================================================================
 
 // require api routes
-require("./app/routing/apiRoutes")(app);
+require('./app/routing/apiRoutes')(app)
 
 // require html routes
-require("./app/routing/htmlRoutes")(app);
+require('./app/routing/htmlRoutes')(app)
 
 // =============================================================================
 // 4. start server by listening to port
 // =============================================================================
 
 // start the server
-app.listen(PORT, function() {
-    console.log("App listening on Port: " + PORT)
-});
+app.listen(PORT, function () {
+  console.log('App listening on Port: ' + PORT)
+})
